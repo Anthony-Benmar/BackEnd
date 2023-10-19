b6559eb1c639334d4c6d548cf786b6cddef676af
Renzo De La Cruz <renzo.delacruz@bbva.com>
2023-10-19 16:34:30 -0500

    Test connection

--- src/main/java/com/bbva/database/MyBatisConnectionFactory.java
+++ src/main/java/com/bbva/database/MyBatisConnectionFactory.java
@@ -1,7 +1,10 @@
 package com.bbva.database;
+
 import com.bbva.fga.core.AppProperties;
 import com.bbva.fga.utils.EnvironmentUtils;
+
 import com.bbva.jetty.MainApp;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.zaxxer.hikari.HikariConfig;
@@ -23,64 +26,53 @@ public class MyBatisConnectionFactory {
 
     private static SqlSessionFactory sqlSessionFactory;
 
-    private MyBatisConnectionFactory() {}
-
-    public static void initialiceInstance(){
+    static {
         try {
-            HikariConfig config = new HikariConfig();
-            Properties properties = AppProperties.getInstance();
-            config.setJdbcUrl(properties.getProperty("database.url"));
-            MainApp.ROOT_LOOGER.log(Level.INFO,"DB - URL: " + properties.getProperty("database.url"));
+            HikariDataSource dataSource = new HikariDataSource();
+            dataSource.setJdbcUrl(AppProperties.getInstance().getProperty("database.url"));
             if (EnvironmentUtils.isLocalEnvironment()) {
-                config.setUsername(properties.getProperty("database.username"));
-                config.setPassword(properties.getProperty("database.password"));
+                dataSource.setUsername(AppProperties.getInstance().getProperty("database.username"));
+                dataSource.setPassword(AppProperties.getInstance().getProperty("database.password"));
             } else {
-                MainApp.ROOT_LOOGER.log(Level.INFO,"DB - USERNAME: " + properties.getProperty("datasource.property.user"));
-                config.setUsername(properties.getProperty("datasource.property.user"));
-                MainApp.ROOT_LOOGER.log(Level.INFO,"DB - PWD: " + properties.getProperty("datasource.property.pwd"));
-                config.setPassword(properties.getProperty("datasource.property.pwd"));
-                MainApp.ROOT_LOOGER.log(Level.INFO,"DB - socketFactory: " + properties.getProperty("datasource.property.socket"));
-                config.addDataSourceProperty("socketFactory",
-                        properties.getProperty("datasource.property.socket"));
-                MainApp.ROOT_LOOGER.log(Level.INFO,"DB - cloudSqlInstance: " +  properties.getProperty("database.cloud_sql_instance"));
-                config.addDataSourceProperty("cloudSqlInstance",
-                        properties.getProperty("database.cloud_sql_instance"));
-                config.addDataSourceProperty("ipTypes", "PUBLIC,PRIVATE");
+                dataSource.addDataSourceProperty(
+                        "user",
+                        AppProperties.getInstance().getProperty("database.username"));
+                dataSource.addDataSourceProperty(
+                        "password",
+                        AppProperties.getInstance().getProperty("database.password"));
+                dataSource.addDataSourceProperty(
+                        "socketFactory",
+                        "com.google.cloud.sql.mysql.SocketFactory"
+                );
+                dataSource.addDataSourceProperty(
+                        "cloudSqlInstance",
+                        AppProperties.getInstance().getProperty("database.cloud_sql_instance")
+                );
             }
-            // config.setAutoCommit(false);
-            config.setMaximumPoolSize(
-                    Integer.parseInt(properties.getProperty("database.maximum_pool_size"))
+            dataSource.setAutoCommit(false);
+            dataSource.setMaximumPoolSize(
+                    Integer.parseInt(AppProperties.getInstance().getProperty("database.maximum_pool_size"))
             );
-            config.setMinimumIdle(
-                    Integer.parseInt(properties.getProperty("database.minimum_idle"))
+            dataSource.setMinimumIdle(
+                    Integer.parseInt(AppProperties.getInstance().getProperty("database.minimum_idle"))
             );
-            config.setConnectionTestQuery("SELECT 1");
+            dataSource.setConnectionTestQuery("SELECT 1");
 
-            
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            String jsonConfigString = gson.toJson(config);
-            MainApp.ROOT_LOOGER.log(Level.INFO,"DB - HIKARI CONFIG: " +  jsonConfigString);
-            HikariDataSource dataSource = new HikariDataSource(config);
-            MainApp.ROOT_LOOGER.log(Level.INFO,"---- TEST ----");
-
-            
             TransactionFactory transactionFactory = new JdbcTransactionFactory();
             Environment environment = new Environment("mysql", transactionFactory, dataSource);
             Configuration configuration = new Configuration(environment);
             configuration.addMappers("com.bbva.database.mappers");
+
             sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
         } catch (Exception e) {
-            MainApp.ROOT_LOOGER.log(Level.INFO,"Error connection Database: " + e.getMessage(), e);
-            MainApp.ROOT_LOOGER.log(Level.SEVERE,"Error connection Database: " + e.getMessage(), e);
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
             sqlSessionFactory = null;
         }
     }
 
+    private MyBatisConnectionFactory() {}
+
     public static SqlSessionFactory getInstance() {
-        if (sqlSessionFactory == null){
-            initialiceInstance();
-        }
         return sqlSessionFactory;
     }
 
