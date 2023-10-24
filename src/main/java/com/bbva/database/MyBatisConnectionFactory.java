6600592dfec8550d0aad34cf009a3e7abac11c33
Renzo De La Cruz <renzo.delacruz@bbva.com>
2023-10-24 14:59:51 -0500

    rollback

--- src/main/java/com/bbva/database/MyBatisConnectionFactory.java
+++ src/main/java/com/bbva/database/MyBatisConnectionFactory.java
@@ -1,9 +1,10 @@
 package com.bbva.database;
-
 import com.bbva.fga.core.AppProperties;
 import com.bbva.fga.utils.EnvironmentUtils;
+import com.bbva.jetty.MainApp;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.Configuration;
@@ -11,63 +12,69 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.transaction.TransactionFactory;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class MyBatisConnectionFactory {
 
+public class MyBatisConnectionFactory {
     private static final Logger LOGGER = Logger.getLogger(MyBatisConnectionFactory.class.getName());
 
     private static SqlSessionFactory sqlSessionFactory;
 
-    static {
-        try {
-            HikariDataSource dataSource = new HikariDataSource();
-            dataSource.setJdbcUrl(AppProperties.getInstance().getProperty("database.url"));
-            if (EnvironmentUtils.isLocalEnvironment()) {
-                dataSource.setUsername(AppProperties.getInstance().getProperty("database.username"));
-                dataSource.setPassword(AppProperties.getInstance().getProperty("database.password"));
-            } else {
-                dataSource.addDataSourceProperty(
-                        "user",
-                        AppProperties.getInstance().getProperty("database.username"));
-                dataSource.addDataSourceProperty(
-                        "password",
-                        AppProperties.getInstance().getProperty("database.password"));
-                dataSource.addDataSourceProperty(
-                        "socketFactory",
-                        "com.google.cloud.sql.mysql.SocketFactory"
-                );
-                dataSource.addDataSourceProperty(
-                        "cloudSqlInstance",
-                        AppProperties.getInstance().getProperty("database.cloud_sql_instance")
-                );
-            }
-            dataSource.setAutoCommit(false);
-            dataSource.setMaximumPoolSize(
-                    Integer.parseInt(AppProperties.getInstance().getProperty("database.maximum_pool_size"))
-            );
-            dataSource.setMinimumIdle(
-                    Integer.parseInt(AppProperties.getInstance().getProperty("database.minimum_idle"))
-            );
-            dataSource.setConnectionTestQuery("SELECT 1");
+    private MyBatisConnectionFactory() {}
 
-            
+    public static void initialiceInstance(){
+        try {
+            HikariConfig config = new HikariConfig();
+            Properties properties = AppProperties.getInstance();
+            config.setJdbcUrl(properties.getProperty("database.url"));
+            if (EnvironmentUtils.isLocalEnvironment()) {
+                config.setUsername(properties.getProperty("database.username"));
+                config.setPassword(properties.getProperty("database.password"));
+            } else {
+                config.setUsername(properties.getProperty("datasource.property.user"));
+                config.setPassword(properties.getProperty("datasource.property.pwd"));
+                config.addDataSourceProperty("socketFactory",
+                        properties.getProperty("datasource.property.socket"));
+                config.addDataSourceProperty("cloudSqlInstance",
+                        properties.getProperty("database.cloud_sql_instance"));
+                config.addDataSourceProperty("ipTypes", "PUBLIC,PRIVATE");
+            }
+            // config.setAutoCommit(false);
+            config.setMaximumPoolSize(
+                    Integer.parseInt(properties.getProperty("database.maximum_pool_size"))
+            );
+            config.setMinimumIdle(
+                    Integer.parseInt(properties.getProperty("database.minimum_idle"))
+            );
+            config.setConnectionTestQuery("SELECT 1");
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String jsonConfigString = gson.toJson(config);
+            MainApp.ROOT_LOOGER.log(Level.INFO,"DB - HIKARI CONFIG: " +  jsonConfigString);
+            HikariDataSource dataSource = new HikariDataSource(config);
+            MainApp.ROOT_LOOGER.log(Level.INFO,"---- TEST ----");
+
             TransactionFactory transactionFactory = new JdbcTransactionFactory();
             Environment environment = new Environment("mysql", transactionFactory, dataSource);
             Configuration configuration = new Configuration(environment);
             configuration.addMappers("com.bbva.database.mappers");
-
             sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
         } catch (Exception e) {
+            MainApp.ROOT_LOOGER.log(Level.INFO,"Error connection Database: " + e.getMessage(), e);
+            MainApp.ROOT_LOOGER.log(Level.SEVERE,"Error connection Database: " + e.getMessage(), e);
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
             sqlSessionFactory = null;
         }
     }
 
-    private MyBatisConnectionFactory() {}
-
     public static SqlSessionFactory getInstance() {
+        if (sqlSessionFactory == null){
+            initialiceInstance();
+        }
         return sqlSessionFactory;
     }
+
 }
