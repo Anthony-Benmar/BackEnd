609429a607fa764f05472ce6eafe869126fea223
Renzo De La Cruz <renzo.delacruz@bbva.com>
2023-10-24 17:57:45 -0500

    test without ip

--- src/main/java/com/bbva/database/MyBatisConnectionFactory.java
+++ src/main/java/com/bbva/database/MyBatisConnectionFactory.java
@@ -40,10 +40,10 @@ public class MyBatisConnectionFactory {
                         properties.getProperty("datasource.property.socket"));
                 config.addDataSourceProperty("cloudSqlInstance",
                         properties.getProperty("database.cloud_sql_instance"));
-                config.addDataSourceProperty("ipTypes", "PUBLIC");
+                // config.addDataSourceProperty("ipTypes", "PUBLIC");
             }
             config.setAutoCommit(false);
-            // config.setMaximumPoolSize(Integer.parseInt(properties.getProperty("database.maximum_pool_size")));
+            config.setMaximumPoolSize(Integer.parseInt(properties.getProperty("database.maximum_pool_size")));
             config.setMinimumIdle(
                     Integer.parseInt(properties.getProperty("database.minimum_idle"))
             );
