776c79a5b966fde2875f7966f9afa0f7638133b1
Gianfranco Solier <gianfranco.solier.contractor@bbva.com>
2024-04-25 15:58:38 -0500

    API GET PROJECT BY DOMAIN ID FIXED

--- src/main/java/com/bbva/dto/project/request/ProjectByDomainIdDTO.java
+++ src/main/java/com/bbva/dto/project/request/ProjectByDomainIdDTO.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 @Setter
 public class ProjectByDomainIdDTO {
     private int projectId;
+    private String sdatoolId;
     private String projectName;
     private int domainId;
 }
