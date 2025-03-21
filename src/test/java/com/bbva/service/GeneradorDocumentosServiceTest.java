4d41153abb071e38b681e68aa6214f0b09318b75
rahit.salas.contractor <rahit.salas.contractor@bbva.com>
2025-03-21 02:22:05 -0500

    PBEXENGDQA-4614 Modificaciones Generador Documentos Mallas

--- src/test/java/com/bbva/service/GeneradorDocumentosServiceTest.java
+++ src/test/java/com/bbva/service/GeneradorDocumentosServiceTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
-public class GeneradorDocumentosServiceTest {
+class GeneradorDocumentosServiceTest {
     private GeneradorDocumentosService generadorDocumentosService;
     private ProjectDao projectDaoMock;
 
