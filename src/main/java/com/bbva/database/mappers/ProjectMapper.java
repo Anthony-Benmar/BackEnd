334289ca5a78277a0a9db8e68380ff29d050f94d
BRIGITTE MELODY MENDEZ PASTOR <brigitte.mendez@bbva.com>
2025-05-18 00:48:51 -0500

    PBEXENGDQA-4614 add: use case in edit project

--- src/main/java/com/bbva/database/mappers/ProjectMapper.java
+++ src/main/java/com/bbva/database/mappers/ProjectMapper.java
@@ -100,7 +100,7 @@ public interface ProjectMapper {
     @Select("CALL SP_UPDATE_PROJECT_INFO(" +
             "#{sdatoolId}, #{projectName}, #{projectDesc}, #{portafolioCode}, #{regulatoryType}, #{ttvType}, #{domainId}, " +
             "#{projectType}, #{categoryType}, #{classificationType}, #{startPiId}, #{endPiId}, #{finalStartPiId}, #{finalEndPiId}, " +
-            "#{wowType}, #{countryPriorityType}, #{statusType}, #{createAuditUser}, #{projectId})")
+            "#{wowType}, #{countryPriorityType}, #{statusType}, #{createAuditUser}, #{projectId}, #{useCaseId})")
     void updateProjectInfo(ProjectInfoDTO dto);
 
     @Delete("Delete from data_project WHERE project_id = #{projectId}")
@@ -253,7 +253,8 @@ public interface ProjectMapper {
             @Result(property = "createAuditDate", column = "create_audit_date"),
             @Result(property = "createAuditUser", column = "create_audit_user"),
             @Result(property = "updateAuditDate", column = "update_audit_date"),
-            @Result(property = "updateAuditUser", column = "update_audit_user")
+            @Result(property = "updateAuditUser", column = "update_audit_user"),
+            @Result(property = "useCaseId", column = "use_case_id")
     })
     List<ProjectInfoSelectResponse> projectInfoFilter(@Param("dto") ProjectInfoFilterRequest dto);
 
