8873a7fff6587fe99dd5cb306a39816ec0cf3528
Diego <diego.amaya.abanto.contractor@bbva.com>
2025-05-22 18:10:17 -0500

    PBEXENGDQA-4614 add test package dtos, fix issue in updateOrInsertUseCase

--- src/test/java/com/bbva/dto/drive/FolderDtoTest.java
+++ src/test/java/com/bbva/dto/drive/FolderDtoTest.java
@@ -6,13 +6,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class FolderDtoTest {
 
     @Test
-    void testFolderDto() {
+    void testFolderDtoAllArgsConstructor() {
+        FolderDto folder = new FolderDto("123", "Test Folder");
+
+        assertEquals("123", folder.getId());
+        assertEquals("Test Folder", folder.getName());
+    }
+
+    @Test
+    void testFolderDtoSettersAndGetters() {
         FolderDto folder = new FolderDto();
+        folder.setId("456");
+        folder.setName("Another Folder");
 
-        folder.setId("folder123");
-        folder.setName("Main Folder");
-
-        assertEquals("folder123", folder.getId());
-        assertEquals("Main Folder", folder.getName());
+        assertEquals("456", folder.getId());
+        assertEquals("Another Folder", folder.getName());
     }
 }
